2abc2cab32b49e840c34510f32a565fe1f2b1168
Brennon Nuckols <brennon@Brennons-MacBook-Pro.local>
2020-01-09 16:29:03 -0700

    tallyUpDonations

--- index.js
+++ index.js
@@ -236,8 +236,11 @@ function getRunnersByTShirtSize(runners, tShirtSize) {
  * @param runners array of runners like the one inside the /data/runners.js file.
  * @returns a number which is the sum of the donations by all runners.
 */
-function tallyUpDonations(/* CODE HERE */) {
-  /* CODE HERE */
+function tallyUpDonations(runners) {
+  let roster = runners.reduce(function(acc, currentValue) {
+    return acc + currentValue.donation;
+  }, 0);
+  return roster;
 }
 
 /////////////// CLOSURES ///////////////
